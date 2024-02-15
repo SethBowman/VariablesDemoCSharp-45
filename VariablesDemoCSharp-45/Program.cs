b551c145a3015ad1aadc3bc93266002fc7320cd3
Seth Bowman <brandonsethbowman@gmail.com>
2024-02-15 12:05:44 -0600

    Add project files.

---
+++ VariablesDemoCSharp-45/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace VariablesDemoCSharp_45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+
+            //Number types
+            //Declaring a variable (creation)
+            //Camel Case - everyNextWordStartingWithCaptital
+            int wholeNumber;
+
+            //Initialize a variable (giving it a value)
+            wholeNumber = 5;
+
+            //Or do both
+            //Declaring and initalizing a variable
+            int otherNumber = 5;
+
+            double avg;
+            avg = 98.7;
+
+            decimal myWallet = 500.00m;
+
+            //Word types
+            string myName = "Seth Bowman";
+
+            char key = '1';
+
+            //Fact type
+            bool isCoding = false;
+
+
+            Console.WriteLine($"Hello my name is {myName}. The whole number {wholeNumber}. My wallet has {myWallet} in it.");
+
+            
+        }
+    }
+}
